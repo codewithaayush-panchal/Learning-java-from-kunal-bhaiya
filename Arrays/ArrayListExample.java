fa34f00f192c527394629cf6c8446c9c3d66c20f
aayushpanchal <codewithaayushofficial@gmail>
2025-12-10 14:02:33 +0530

    Arrays and Arraylist done

---
+++ Arrays/ArrayListExample.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in) ;
+
+        // Syntax
+        ArrayList<Integer> list = new ArrayList<>(5) ;
+
+//        list.add(29358);
+//        list.add(798);
+//        list.add(835);
+//        list.add(29358);
+//        list.add(798);
+//        list.add(29358);
+//        list.add(798);
+//        list.add(835);
+//        list.add(292248);
+
+//        System.out.println(list.contains(835));
+
+//        System.out.println(list);
+//        list.set(0, 99) ;
+//        list.remove(2);
+//        System.out.println(list);
+
+        // input
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt()) ;
+        }
+
+        // get item at any index
+        for (int i = 0; i < 5 ; i++) {
+            System.out.print(list.get(i) + " " );       // pass index here, list[index] syntax will not work here
+        }
+
+        System.out.println(list);
+
+    }
+
+}
