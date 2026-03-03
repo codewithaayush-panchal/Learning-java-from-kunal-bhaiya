c710db4897acbc7e81d4faecbd5ac4c4de87b422
aayushpanchal <codewithaayushofficial@gmail>
2026-03-03 22:01:20 +0530

    Recursion-1 done

---
+++ Recursion_1/Fibo_recur.java
@@ -0,0 +1,17 @@
+
+
+public class Fibo_recur {
+    public static void main(String[] args) {
+        int ans = fibo(4);
+        System.out.println(ans);
+
+    }
+
+    static int fibo(int n) {
+        // base condition
+        if( n < 2 ) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
