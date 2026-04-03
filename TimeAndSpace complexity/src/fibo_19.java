e78ffa991d4f82800d1156ccab80d738c71dbd70
aayushpanchal <codewithaayushofficial@gmail>
2026-04-03 22:23:09 +0530

    complexity done

---
+++ TimeAndSpace complexity/src/fibo_19.java
@@ -0,0 +1,25 @@
+
+
+public class fibo_19 {
+    public static void main(String[] args) {
+        int ans = fibo(4);
+        System.out.println(ans);
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fiboFormula(i));
+        }
+
+    }
+
+    static int fiboFormula(int n) {
+        // just for demo use long instead
+        return (int)(Math.pow((1+Math.sqrt(5))/2, n) / Math.sqrt(5)) ;
+    }
+
+    static int fibo(int n) {
+        // base condition
+        if( n < 2 ) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
